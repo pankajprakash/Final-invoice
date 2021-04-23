9da4ecba1415121f9c4f65f94ab44cafc416fd91
KeyMouseIT <you@example.com>
2021-04-23 13:12:59 +0530

    total price as per tax and prodcuts has been done

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,58 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
-import Datepicker from './Datepicker';
-import { Container, Row, Col } from 'reactstrap';
+import Datepicker from "./Datepicker";
+import { Container, Row, Col } from "reactstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useSelector, useDispatch } from 'react-redux'
-import { postInvoiceData } from './../Redux/Action/FormAction'
-import { CompanyData } from './../Redux/Action/ToCompanies'
+import { useSelector, useDispatch } from "react-redux";
+import { postInvoiceData } from "./../Redux/Action/FormAction";
+import { CompanyData } from "./../Redux/Action/ToCompanies";
 import { postLoginData } from "./../../src/Redux/Action/LoginAction";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import Select from 'react-select';
+import Select from "react-select";
 
-import { productData } from './../Redux/Action/ProductAction'
+import { productData } from "./../Redux/Action/ProductAction";
 import { getAllByText } from "@testing-library/dom";
 
 const Form = ({ history, location }) => {
-
-
-
   const [state1, setstate1] = useState({
-    "from": '',
-    "to": '',
-    "createdBy": '',
-    "items": {
-      "productName": '',
-      "quantity": '',
-      "description": '',
-      "unitPrice": '',
-      "total": ''
+    from: "",
+    to: "",
+    createdBy: "",
+    items: {
+      productName: "",
+      quantity: "",
+      description: "",
+      unitPrice: "",
+      total: "",
     },
-    "dueDate": '',
-    "status": ''
-  })
+    dueDate: "",
+    status: "",
+  });
 
   const [addMore, setAddmore] = useState({
     items: [
       {
-        "productName": "",
-        "quantity": "",
-        "description": "",
-        "unitPrice": "",
-        "total": "",
+        productName: "",
+        quantity: "",
+        description: "",
+        unitPrice: "",
+        total: "",
       },
     ],
   });
 
   const addItems = (e) => {
-
     // setAddmore((prevState) => ({
 
     //   //here hName was newCon
@@ -68,171 +64,139 @@ const Form = ({ history, location }) => {
     //   ],
     // }));
 
-
-
     setAddmore({
       items: [
         {
-          "productName": "",
-          "quantity": "",
-          "description": "",
-          "unitPrice": "",
-          "total": "",
+          productName: "",
+          quantity: "",
+          description: "",
+          unitPrice: "",
+          total: "",
         },
       ],
-    })
-
-
+    });
   };
 
-
-
-
-
-  const state = useSelector(state => state)
-  const dispatch = useDispatch()
-  const { control, register, handleSubmit, formState: { errors } } = useForm();
-
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     dispatch(CompanyData());
     // dispatch(postLoginData())
-    dispatch(productData())
-    console.log(location, "user reg data in form")
-    console.log(selectedOrg, "sel org")
-    console.log(selectedOrg, "gggggggggggggggggggggggggggggggggggggggggggggg")
+    dispatch(productData());
+    console.log(location, "user reg data in form");
+    console.log(selectedOrg, "sel org");
+    console.log(selectedOrg, "gggggggggggggggggggggggggggggggggggggggggggggg");
     // console.log(selOrg,"local s data")
-  }, [])
+  }, []);
   // const onSubmit = (invoicedata) => dispatch(postInvoiceData
-
 
   //   (invoicedata));
   const getValues = (invoicedata) => {
     setstate1({
-      "from": invoicedata.from,
-      "to": invoicedata.to,
-      "createdBy": invoicedata.createdBy,
-      "items": {
-        "productName": invoicedata.ProductName,
-        "quantity": invoicedata.quantity,
-        "description": invoicedata.Description,
-        "unitPrice": invoicedata.unitPrice,
-        "total": invoicedata.total
+      from: invoicedata.from,
+      to: invoicedata.to,
+      createdBy: invoicedata.createdBy,
+      items: {
+        productName: invoicedata.ProductName,
+        quantity: invoicedata.quantity,
+        description: invoicedata.Description,
+        unitPrice: invoicedata.unitPrice,
+        total: invoicedata.total,
       },
-      "dueDate": invoicedata.dueDate,
-      "status": invoicedata.status
-    })
+      dueDate: invoicedata.dueDate,
+      status: invoicedata.status,
+    });
+
+    setTimeout(() => { }, 500);
+  };
+
+  const onSubmit = (invoicedata) => {
+
+    //  getValues(invoicedata)
+    dispatch(postInvoiceData(invoicedata, history))
+    console.log(invoicedata, "invouice data itemsss");
 
     setTimeout(() => {
 
-    }, 500);
+      history.push("/download")
+
+    }, 1000);
 
 
-  }
 
-
-  const onSubmit = (invoicedata) => {
-    
-  // const alldata={...invoicedata,
-  // productName:pro.map(e => e.label)}
-  //   //  getValues(invoicedata)
-  //   dispatch(postInvoiceData(alldata, history))
-    console.log(invoicedata, "invouice data itemsss")
-
-
-    // setTimeout(() => {
-
-    //   history.push("/download")
-    // }, 1000);
-    // 
-    // console.log(object)
-
-    // console.log("dispatched data",state.companyId.to);
-
-  }
-
+    console.log("dispatched data", state.companyId.to);
+  };
 
   const logoutFun = () => {
     localStorage.clear();
     history.push("/");
-  }
+  };
 
   // for organisation
-  const selectedOrg = JSON.parse(localStorage.getItem("selected_company"))
+  const selectedOrg = JSON.parse(localStorage.getItem("selected_company"));
 
-  const [pro, setProducts] = useState([])
+  const [pro, setProducts] = useState([]);
   const [tax, setTax] = useState({
-    tax: '',
-    category: ''
-  })
+    tax: "",
+    category: "",
+  });
   //used new use selector for for product data
-  const productsData = useSelector(state => state.products.Product)
+  const productsData = useSelector((state) => state.products.Product);
 
- 
-
-
-
-  
-  const mappedProducts = productsData.map((e) =>
-
-  ({
+  const mappedProducts = productsData.map((e) => ({
     label: e.productName,
     value: e.id,
-    color: '#00B8D9',
-    category:e.category,
-    tax:e.tax
-  })
-  )
-console.log(mappedProducts)
+    color: "#00B8D9",
+    category: e.category,
+    tax: e.tax,
+  }));
+  console.log(mappedProducts);
 
+  const allInvoice = () => {
+    history.push("/download");
+  };
 
-const allInvoice = () =>{
-  history.push("/download")
-}
+  const [total, setTotal] = useState({
+    quantity: "",
+    unitPrice: "",
+    total: "",
+  });
 
+  const setQuan = (e) => {
+    setTotal({ ...total, [e.target.name]: e.target.value });
+  };
 
-const[total,setTotal] = useState({
-  "quantity":'',
-  "unitPrice":'',
-  "total": ''
-
-})
-
-const setQuan = (e) => {
-  setTotal({...total,
- [e.target.name]:e.target.value,
- 
-  })
-  
-}
-
-const setTotalVal = () => {
-  setTotal({
-   
-      'total': total.quantity * total.unitPrice
-   
-     
-  })
-}
+  const setTotalVal = () => {
+    setTotal({
+      total: total.quantity * total.unitPrice,
+    });
+  };
 
   return (
-
     <>
-
-
- <div className="container-area">
+      <div className="container-area">
         <div className="header">
           <div className="navbar">
             <div className="left-side">
-            <i class="fas fa-user-alt"></i>
+              <i class="fas fa-user-alt"></i>
               <div className="all-invoice-btn">
-                <button onClick={()=>allInvoice()} className="addnew-btn">Show Invoices</button>
+                <button onClick={() => allInvoice()} className="addnew-btn">
+                  Show Invoices
+                </button>
               </div>
             </div>
             <div className="right-side">
               <div className="logout-btn">
-              <button className="addnew-btn" onClick={logoutFun}><i class="far fa-plus-square"> logout </i></button>
-
+                <button className="addnew-btn" onClick={logoutFun}>
+                  Logout <i class="fas fa-sign-out-alt"></i>
+                </button>
               </div>
             </div>
           </div>
@@ -240,51 +204,44 @@ const setTotalVal = () => {
       </div>
 
       <Container>
-
         <div className="outer">
-
           {/* {JSON.stringify(productsData,"product-data")} */}
-         
 
           <form onSubmit={handleSubmit(onSubmit)} className="form-data">
-
-
             <Row>
               <div className="btn-head1">
                 <div>
-                  <h5 className="top-head"><b >INVOICE MANAGEMENT</b></h5>
+                  <h5 className="top-head">
+                    <b>INVOICE MANAGEMENT</b>
+                  </h5>
                   {/* {productsData.map((e) => (e.productName)
 
                   )} */}
                 </div>
 
-                <div className="log-btn">
-                  <button className="addnew-btn" onClick={logoutFun}><i class="far fa-plus-square"> logout </i></button>
-                </div>
+                {/* <div className="log-btn">
+                  <button className="addnew-btn" onClick={logoutFun}>
+                    <i class="far fa-plus-square"> logout </i>
+                  </button>
+                </div> */}
               </div>
             </Row>
-
-
 
             <Row className="firstRow">
               <Col md="4">
                 <label for="status">From</label>
                 <select {...register("from")} className="select1">
-
                   {selectedOrg.map((e) => (
                     <option value={e.id}>{e.companyName}</option>
                   ))}
-
-
                 </select>
-
               </Col>
               <Col md="4">
                 <label for="Created By">Created By </label>
                 <input
                   type="text"
-                  {...register('createdBy', {
-                    required: true
+                  {...register("createdBy", {
+                    required: true,
                   })}
                 />
 
@@ -294,37 +251,28 @@ const setTotalVal = () => {
               </Col>
               <Col md="4">
                 <label for="To">To</label>
-                <select {...register("to", { required: true })} className="select">
-
-                  {state.companyId.to.map((e) =>
+                <select
+                  {...register("to", { required: true })}
+                  className="select"
+                >
+                  {state.companyId.to.map((e) => (
                     <option value={e.id}>{e.name}</option>
-                  )}
-
+                  ))}
                 </select>
                 <p className="para">
                   {errors.to && "this field can't be empty*"}
                 </p>
-
-
               </Col>
             </Row>
 
-            <Row className="firstRow">
+            <Row className="">
               <Col md="4">
                 <label for="Notes">Notes</label>
-                <input
-                  type="text"
-                  {...register('Notes', {
-
-                  })}
-                />
+                <input type="text" {...register("Notes", {})} />
                 <p className="para">
                   {errors.Notes && "this field can't be empty*"}
                 </p>
               </Col>
-
-
-
 
               <Col md="4  ">
                 <label for="dueDate">Due Date</label>
@@ -337,26 +285,32 @@ const setTotalVal = () => {
                   }
                   />  */}
 
-
-                {<Controller name="dueDate" control={control} defaultValue={null}
-                  render={
-                    (p) => {
-                      console.log(`onChange, value`, p.fields)
-                      return <DatePicker selected={p.field.value}
-                        placeholderText="select date   " onChange={p.field.onChange} className="date-picker" />
-
-                    }
-
-                  }
-                />
+                {
+                  <Controller
+                    name="dueDate"
+                    control={control}
+                    defaultValue={null}
+                    render={(p) => {
+                      console.log(`onChange, value`, p.fields);
+                      return (
+                        <DatePicker
+                          selected={p.field.value}
+                          placeholderText="select date   "
+                          onChange={p.field.onChange}
+                          className="date-picker"
+                        />
+                      );
+                    }}
+                  />
                 }
-
-
               </Col>
 
               <Col md="4">
                 <label for="status">Status</label>
-                <select {...register("status", { required: true })} className="select">
+                <select
+                  {...register("status", { required: true })}
+                  className="select"
+                >
                   <option value="pending">Pending</option>
                   <option value="underReview">Under Review</option>
                   <option value="approved">Approved</option>
@@ -368,22 +322,13 @@ const setTotalVal = () => {
               </Col>
             </Row>
 
-
-            <Row className="firstRow">
-
-
-
-
-            </Row>
+            <Row className=""></Row>
 
             {/*mapping newly created data */}
 
-
             <Row className="first">
-
-            {/* {JSON.stringify(pro,"tax vali state")} */}
+              {/* {JSON.stringify(pro,"tax vali state")} */}
               <Col md="4">
-
                 <label for="product name">Product Name</label>
 
                 {/* <select {...register("productName", { required: true })} className="select">
@@ -400,21 +345,18 @@ const setTotalVal = () => {
                   isMulti
                   control={control}
                   render={(p) => {
-                    const { ref, onChange, value } = p.field
+                    const { ref, onChange, value } = p.field;
                     return (
                       <Select
                         inputRef={ref}
                         classNamePrefix="addl-class"
-                        options={mappedProducts}                   
-                        value={mappedProducts.find(e => (e.value === value))}
-                        onChange={val => setProducts(val)}
-
+                        options={mappedProducts}
+                        value={mappedProducts.find((e) => e.value === value)}
+                        onChange={(val) => setProducts(val)}
                       />
-                    )
+                    );
                   }}
                 />
-
-
 
                 <p className="para">
                   {errors.to && "this field can't be empty*"}
@@ -422,41 +364,31 @@ const setTotalVal = () => {
               </Col>
 
               <Col md="4">
-            
-              <label for="Quantity">Category</label>
-              <input
+                <label for="Quantity">Category</label>
+                <input
                   type="text"
                   value={pro.category}
                   placeholder="Category"
                   disabled
-                /> 
+                />
               </Col>
 
               <Col md="4">
-              <label for="Quantity">Tax</label>
-      
+                <label for="Quantity">Tax</label>
 
-              <input
-                  type="text"
-                  value={pro.tax}
-                  placeholder="Tax"
-                  disabled
-                /> 
+                <input type="text" value={pro.tax} placeholder="Tax" disabled />
               </Col>
 
-
               <Col md="4">
-                {JSON.stringify(total)}
+                {/* {JSON.stringify(total)} */}
                 <label for="Quantity">Quantity</label>
                 <input
                   type="number"
                   name="quantity"
                   value={total.quantity}
                   {...register("quantity", {
-                    required: true
-
+                    required: true,
                   })}
-                  
                   onChange={setQuan}
                   placeholder="enter quantity"
                 />
@@ -469,10 +401,10 @@ const setTotalVal = () => {
                 <label for="description">Description</label>
                 <input
                   type="text"
-                  {...register('Description', {
-                    required: true
-
+                  {...register("Description", {
+                    required: true,
                   })}
+                  placeholder="Description"
                 />
                 <p className="para">
                   {errors.to && "this field can't be empty*"}
@@ -485,82 +417,55 @@ const setTotalVal = () => {
                   type="number"
                   name="unitPrice"
                   value={total.unitPrice}
-                  
-                  {...register('unitPrice', {
-                    required: true
-
+                  {...register("unitPrice", {
+                    required: true,
                   })}
-
-                  onChange={(e) => { setQuan(e); setTimeout(() => {
-                    setTotalVal();
-                  }, 1000);} }
-                 
+                  onChange={(e) => {
+                    setQuan(e);
+                  }}
                 />
                 <p className="para">
                   {errors.to && "this field can't be empty*"}
                 </p>
               </Col>
-
 
               <Col md="4">
                 <label for="Total">Total</label>
                 <input
                   type="number"
-                  {...register('total', {
-                    required: true
-
+                  {...register("total", {
+                    required: true,
                   })}
-                  
-                  value={total.total}
-                  // onClick={}
-                  
+                  value={
+                    ((parseFloat(total.unitPrice) || 0) *
+                      (parseFloat(total.quantity) || 0)) * (1 + (0.01) * (parseFloat(pro.tax) || 0))
+                  }
+                // onClick={}
                 />
                 <p className="para">
                   {errors.to && "this field can't be empty*"}
                 </p>
               </Col>
-
-            
-
-
-
-
             </Row>
-
-
-
-
-
-
-
-
-
-
-
 
             <Row className="firstRow">
-              <Col className="button-column"  >
-                <button className="addnew-btn" onClick={() => addItems()}><i class="far fa-plus-square"> Add More </i></button>
+              <Col className="button-column">
+                <button className="addnew-btn" onClick={() => addItems()}>
+                  <i class="far fa-plus-square"> Add More </i>
+                </button>
               </Col>
-
             </Row>
-
-
 
             <Row className="firstRow">
-              <Col className="button-column"  >
-                <button className="submit-btn" >SUBMIT</button>
+              <Col className="button-column">
+                <button className="submit-btn">SUBMIT</button>
               </Col>
-
             </Row>
-
           </form>
         </div>
       </Container>
-
     </>
-  )
-}
+  );
+};
 
-export default Form
-
+export default Form;
